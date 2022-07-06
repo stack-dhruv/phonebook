593571cbebe93ab0c79803f41380c8f42d251fb4
stack-dhruv <iamdhruv1563@gmail.com>
2022-07-06 22:58:16 +0530

    Setup for application front-end ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AddPersonComponent } from './component/add-person/add-person.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ContactCardComponent } from './component/contact/contact-card/contact-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddPersonComponent,
+    ContactComponent,
+    ContactCardComponent
   ],
   imports: [
     BrowserModule,
